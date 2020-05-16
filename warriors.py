57d0a32df79fc141000f8e900e6b9ccd13dced49
David Rojas <colombianclimber@gmail.com>
2020-05-15 21:02:07 -0700

    moved to flask for site

--- warriors.py
+++ warriors.py
@@ -34,6 +34,16 @@ def trainingpacecal():
 def pacechart():
     return render_template('pacechart.html')
 
+@app.route('/thankyou')
+def thankyou():
+    first = request.args.get('first')
+    last = request.args.get('last')
+    return render_template('thankyou.html', first=first, last=last)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 
 if __name__ == '__main__':
     app.run(debug=True)
