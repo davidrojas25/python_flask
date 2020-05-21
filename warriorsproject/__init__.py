1b103b2a42d63f4b54dc8f2b81be88fc8e35068b
David Rojas <colombianclimber@gmail.com>
2020-05-20 18:16:54 -0700

    changed app.config to testing false

--- warriorsproject/__init__.py
+++ warriorsproject/__init__.py
@@ -9,6 +9,7 @@ login_manager = LoginManager()
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = "mysecretkey"
+app.config['TESTING'] = False
 ### SQL DB Section #####
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
